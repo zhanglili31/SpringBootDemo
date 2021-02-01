fca0db01c274d84011afd69b49857381b2a42580
zhanglili31 <zhanglili31@meituan.com>
2021-02-01 16:44:29 +0800

    merge

---
+++ complete/src/main/java/com/example/schedulingtasks/ScheduleConfig/config.java
@@ -0,0 +1,9 @@
+package com.example.schedulingtasks.ScheduleConfig;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+
+//@Component
+//@EnableScheduling
+public class config {
+}
